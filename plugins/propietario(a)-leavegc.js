958db3fd7f91535fc99a164b42dc0f052521566e
D.E.S.N_OFFICIAL17 <131302001+Saldarriaga184@users.noreply.github.com>
2023-06-02 23:50:00 -0500

    Actualizar propietario(a)-leavegc.js

--- plugins/propietario(a)-leavegc.js
+++ plugins/propietario(a)-leavegc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-await conn.reply(id, '*GATABOT ABANDONA EL GRUPO FUE UN GUSTO ESTA AQUI HASTA PRONTO 👋*') 
+await conn.reply(id, '*𝙆𝙖𝙣𝙖𝙠𝙞𝘽𝙤𝙩-𝙈𝘿 ABANDONA EL GRUPO FUE UN GUSTO ESTA AQUI HASTA PRONTO 👋*') 
 await conn.groupLeave(id)}
 handler.command = /^(salir|leavegc|salirdelgrupo|leave)$/i
 handler.group = true
