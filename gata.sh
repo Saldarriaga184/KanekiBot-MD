370c0e7a231b1bfe3be0d3ac5d76491e60a1707d
D.E.S.N_OFFICIAL17 <131302001+Saldarriaga184@users.noreply.github.com>
2023-06-06 00:27:43 -0500

    Actualizar gata.sh

--- gata.sh
+++ gata.sh
@@ -249,7 +249,7 @@ v1="${b}\033[32m"
 b="\033[0m"
 
 menu() {
-	#KanakiBot-MD
+	#GataBot-MD
 	echo -e "${v1} MENÚ DE CONFIGURACIÓN"
 	printf "\n"
 	printf "${v1}[${b}01${v1}]${v} Visistar Grupo de Actualizaciones\n"
