ded88deb055f0523884f6f0dfaae77e9b98dd6bf
D.E.S.N_OFFICIAL17 <131302001+Saldarriaga184@users.noreply.github.com>
2023-06-07 19:35:57 -0500

    Actualizar grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -13,7 +13,7 @@ throw `
 ${lenguajeGB['smsAvisoMG']()}*╭━[ ${wm} ]━⬣*
 *┃➥ ${usedPrefix + command} abrir*
 *┃➥ ${usedPrefix + command} cerrar*
-*╰━━━━━[ 𓃠 ${vs} ]━━━━━⬣*
+*╰━━━━━[ ✿︎ ${vs} ]━━━━━⬣*
 `.trim()
 await conn.groupSettingUpdate(m.chat, isClose)
   
